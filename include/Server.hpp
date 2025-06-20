44f8a1b0ace8b521df4f35f9d94202869fde34b7
42Alena <livionak@gmail.com>
2025-06-20 23:28:55 +0200

    Server: initial poll() event handling

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,40 +6,53 @@
 /*   By: akurmyza <akurmyza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:31:22 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/18 12:29:37 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/06/20 22:35:16 by akurmyza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
-#include <string>
 
-#include <cerrno>//Errors
-#include <cstring>//Errors
+#include <string>
+#include <vector>
+
+#include <cerrno>  //Errors
+#include <cstring> //Errors
 
 #include <sys/socket.h> //socket,bind,listen
-#include <sys/types.h>    //socket
+#include <sys/types.h>	//socket
 #include <sys/socket.h>
 
-#include <fcntl.h> //fcntl
+#include <fcntl.h>		//fcntl
 #include <netinet/in.h> //struct sockaddr_in(IPv4)
-#include <arpa/inet.h> //htons(convert hot<=>bytes)
-#include <poll.h> //poll
-#include <map> //std::map https://en.cppreference.com/w/cpp/container/map.html
-#include <cstdlib>  //EXIT_SUCCESS, EXIT_FAILURE
+#include <arpa/inet.h>	//htons(convert hot<=>bytes)
+#include <poll.h>		//poll
+#include <map>			//std::map https://en.cppreference.com/w/cpp/container/map.html
+#include <cstdlib>		//EXIT_SUCCESS, EXIT_FAILURE
+
+// from  man poll()
+#include <fcntl.h>	   //  open()
+#include <poll.h>	   //  poll() and struct pollfd
+#include <stdio.h>	   //  printf(), fprintf()
+#include <stdlib.h>	   //  malloc(), exit()
+#include <sys/types.h> //  ssize_t
+#include <unistd.h>	   //  read(), close()
 
 #include "Client.hpp"
 
-class Server{
-	private:
-	
+class Server
+{
+private:
 	int _port;
 	std::string _password;
-	int _server_fd;    //socket()
-	// std::vector<struct pollfd> _poll_fds; 
+	int _server_fd; // sserver socket()
+	std::vector<struct pollfd> _poll_fds;
+
 	std::map<int, Client> _clients;
-	
-	public:
+
+	static void Server::throwRuntimeErr(const std::string& errMsg) ;
+
+public:
 	Server();
 	Server(int &port, std::string &password);
 	Server(const Server &o);
@@ -47,9 +60,9 @@ class Server{
 	~Server();
 
 	int run();
-	
-	//getter
+
+	//_________GETTER______________
 	int getPort() const;
 	std::string getPassword() const;
 };
-std::ostream& operator<<(std::ostream &os, const Server &o );
+std::ostream &operator<<(std::ostream &os, const Server &o);
--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: akurmyza <akurmyza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:31:25 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/18 12:29:58 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/06/20 23:25:41 by akurmyza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 Server::Server() : _port(0),
 				   _password(""),
 				   _server_fd(-1) {}
+
 Server::Server(int &port, std::string &password) : _port(port),
 												   _password(password),
 												   _server_fd(-1) {}
@@ -51,16 +52,16 @@ https://man7.org/linux/man-pages/man3/inet_aton.3.html
 INADDR_ANY (0.0.0.0)              means any address for socket binding;
 IPv4														https://man7.org/linux/man-pages/man7/ip.7.html
 
-  struct in_addr {	uint32_t       s_addr;     }; // address in network byte order
+struct in_addr {	uint32_t       s_addr;     }; // address in network byte order
 htons()  converts the unsigned short integer hostshort  from host byte order to network byte order.  https://man7.org/linux/man-pages/man3/htons.3.html
 
 listen - listen for connections on a socket https://man7.org/linux/man-pages/man2/listen.2.html
 poll - wait for some event on a file descriptor https://man7.org/linux/man-pages/man2/poll.2.html
-         struct pollfd {
-               int   fd;         // file descriptor/
-               short events;     //requested events  
-               short revents;    // returned events  
-           };
+		 struct pollfd {
+			   int   fd;         // file descriptor/
+			   short events;     //requested events
+			   short revents;    // returned events
+		   };
 */
 int Server::run()
 {
@@ -101,54 +102,71 @@ int Server::run()
 	{
 		throw std::runtime_error(std::string("Failed to listen: ") + strerror(errno));
 	}
-/* Add _server_fd to _poll_fds
-Infinite loop with poll()
-If _server_fd is ready → accept() a new connection
-Add the new client to _clients and _poll_fds
-If a client socket is ready → read or disconnect */
 
-// my private: std::vector<struct pollfd> _poll_fds;
-	//int poll(struct pollfd *fds, nfds_t nfds, int timeout=0=NonBlocking);
-	// struct pollfd {
-	// 	int   fd;         // file descriptor/
-	// 	short events;     //requested events  
-	// 	short revents;    // returned events  
-	// };
-	/* poll() should block waiting for a file descriptor to become ready.
-       The call will block until either:
-       •  a file descriptor becomes ready;
-       •  the call is interrupted by a signal handler; or
-       •  the timeout expires.
-	    */
-	// poll() performs a similar task to select(2): it waits for one of a	set of file descriptors to become ready to perform I/O.
-	//int poll(struct pollfd *fds, nfds_t nfds, int timeout);
 	// poll(struct pollfd *fds,number fds, timeout=secToWait=0=NonBlocking=returnImmediatly). ret>=0 || -1 errno
 	//.data() comes from  <vector>. Ret:  a pointer to the first element (T*)||  NULL if empty.
-	//number fds: _poll_fds.size()
-	// int pollResult = poll(_poll_fds.data(), _poll_fds.size(),   0); 
-	// if (pollResult == -1)
-	// {
-	// 	throw std::runtime_error(std::string("Failed to wait for event with  poll(): ") + strerror(errno));
-	// }
+	// number fds: _poll_fds.size()
+	int pollResult = poll(_poll_fds.data(), _poll_fds.size(), 0);
+	if (pollResult == -1)
+	{
+		throw std::runtime_error(std::string("Failed to wait for event with  poll(): ") + strerror(errno));
+	}
+
 	// for (int i =0; i <=  _poll_fds.size(); i++){
 	// 	if(_poll_fds.revents() &&)
 	// }
-	
-//code from man poll():
-//nfds_t is the type used by the poll() function to represent the number of file descriptors in the pollfd array.
-// for (nfds_t j = 0; j < nfds; j++) {
-// 	if (pfds[j].revents != 0) {
-// 		printf("  fd=%d; events: %s%s%s\n", pfds[j].fd,
-// 			   (pfds[j].revents & POLLIN)  ? "POLLIN "  : "",
-// 			   nfds_t
 
-			  
+	// Add _server_fd to _poll_fds
+	//  my : std::vector<struct pollfd> _poll_fds;
+	// my: int _server_fd;    //sserver socket()
+
+	struct pollfd server_pollfd;
+	server_pollfd.fd = _server_fd;
+	server_pollfd.events = POLLIN; // POLLIN - There is data to read
+	server_pollfd.revents = 0;	   // 0 =poll(0fill with result)
+
+	_poll_fds.push_back(server_pollfd);
+
+	// Main loop + basic poll() usage	15–20 min	Includes poll(), loop structure, error check
+	//  Check which fds have events
+
+	nfds_t fdIndex = 0;
+	while (fdIndex < _poll_fds.size())
+	{
+
+		if (_poll_fds[fdIndex].revents & POLLIN)
+		{
+			char buf[10];
+			ssize_t readBytes = read(_poll_fds[fdIndex].fd, buf, sizeof(buf));
+			if (readBytes == -1)
+				throwRuntimeErr("read"); // Exit if read fails
+			fdIndex++;
+			printf("   Read %zd bytes: %.*s\n", readBytes, (int)readBytes, buf); // Show what was read
+		}
+		else if (_poll_fds[fdIndex].revents & (POLLHUP | POLLERR)) // error socket ||client disconnected
+		{
+
+			printf("    Closing fd %d\n", _poll_fds[fdIndex].fd);
+			if (close(_poll_fds[fdIndex].fd) == -1)
+			{
+				_poll_fds.erase(_poll_fds.begin() + fdIndex);
+				throwRuntimeErr("close"); // Exit if close fails
+			}
+		}
+		else
+		{
+			fdIndex++;
+		}
+	}
+
+	/*
+	Infinite loop with poll()
+	If _server_fd is ready → accept() a new connection
+	Add the new client to _clients and _poll_fds
+	If a client socket is ready → read or disconnect */
+
 	return EXIT_SUCCESS;
 }
-
-
-
-
 
 //________________getter_________________________________
 
@@ -162,7 +180,7 @@ std::string Server::getPassword() const
 	return _password;
 }
 
-std::ostream& operator<<(std::ostream &os, const Server &o)
+std::ostream &operator<<(std::ostream &os, const Server &o)
 {
 
 	os << "Port: " << o.getPort() << std::endl;
@@ -170,4 +188,10 @@ std::ostream& operator<<(std::ostream &os, const Server &o)
 
 	os << "Password: " << o.getPassword() << std::endl; // TODO: hide. ONLY FOR DEBUG now
 	return os;
+}
+
+//______ HELPER FUNCTIONS____________________
+void Server::throwRuntimeErr(const std::string &errMsg)
+{
+	throw std::runtime_error("Error in " + errMsg + " => " + std::string(strerror(errno)));
 }
