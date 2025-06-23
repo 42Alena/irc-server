ec516657cc667c07cc9682f90bbeb6206b6597e0
Numbersdontlie <luifer92@gmail.com>
2025-06-23 17:03:50 +0200

    Working on the client class definition and methods implementation

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,25 +6,34 @@
 /*   By: lperez-h <lperez-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:31:16 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/23 14:47:34 by lperez-h         ###   ########.fr       */
+/*   Updated: 2025/06/23 15:54:45 by lperez-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "Server.hpp"
-#pragma once
-
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
 #include <iostream>
 
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include "Server.hpp"
+#include "Channel.hpp"
+
 
 class Client
 {
 private:
-	int _fd;				   // read,write,clients in poll,close(fd). -1=nosocket
-	std::string _nickName;	   // NICK, private msg
-	std::string _receivedData; // temp stores data from the client (accumulates with each recv() until \r\n).
+	int _fd;					// read,write,clients in poll,close(fd). -1=nosocket
+	std::string _nickName;		// NICK, private msg
+	std::string _user; 			// USER, private msg
+	bool _registered;			// true if USER+NICK+PASS false otherwise
+	std::vector<std::string> _channels;	// to track the channels the client has joined
+	std::map<std::string, bool> _channelOps;	// to track the channels where the client is an operator
+	std::string _sendData;	// temp stores data to be sent to the client (accumulates with each send() until \r\n).
+	std::string _receivedData;	// temp stores data from the client (accumulates with each recv() until \r\n).
 	// for LATER...
 	//  std::string _username;  //USER, private msg
 	// bool _registered;  //USER+NICK+PASS
@@ -41,14 +50,14 @@ public:
 	// to get the file descriptor of the client
 	int getFd() const;
 	// to get the nickname of the client
-	const std::string &getNickname() const;
+	std::string getNickname() const;
 	//to get the username of the client
 	std::string getUser() const;
 
 	//to check if the client is registered
 	bool isRegistered() const;
 	//to check if the client is an operator
-	bool isOperator() const;
+	bool isOperator(const std::string& channel) const;
 	//to join a channel
 	void joinChannel(const std::string& channel);
 	//to leave a channel
@@ -77,3 +86,5 @@ public:
 	std::string extractNextCmd();
 };
 const std::ostream &operator<<(std::ostream &os, const Client &o);
+
+#endif
