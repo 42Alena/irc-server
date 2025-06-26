7a65372266fd0cecb72e25dbbbc6904b19bd8fa2
Alena <livionak@gmail.com>
2025-06-26 14:49:36 +0200

    fixed: compiling, added: git actions

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -6,49 +6,49 @@
 /*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:42:32 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/26 13:10:22 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/06/26 14:46:22 by akurmyza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
-# define CHANNEL_HPP
+#define CHANNEL_HPP
 
 #include <string>
 #include <vector>
 #include "colors.hpp" /// Alena: replaced hardcoded color defines with colors.hpp
-#include <map>  // Alena: added for map
+#include <map>		  // Alena: added for map
 
 // TODO (Alena): added forward declaration Client class before use it
 class Client;
-class Channel 
+class Channel
 {
-	private:
-		std::string _name; // Name of the channel, must start with '#'
-		std::string _topic; // Topic of the channel
-		std::vector<Client*> _members; // Vector to store members of the channel
-		std::vector<Client*> _operators; // Vector to store operators of the channel
-		std::map<char, bool> _modes; // Map to store channel modes (e.g., 'i' for invite-only, 't' for topic settable by operators only)
-		std::string _key; // Key for the channel, if set
-		int _userLimit; // Limit on the number of users in the channel
+private:
+	std::string _name;				  // Name of the channel, must start with '#'
+	std::string _topic;				  // Topic of the channel
+	std::vector<Client *> _members;	  // Vector to store members of the channel
+	std::vector<Client *> _operators; // Vector to store operators of the channel
+	std::map<char, bool> _modes;	  // Map to store channel modes (e.g., 'i' for invite-only, 't' for topic settable by operators only)
+	std::string _key;				  // Key for the channel, if set
+	int _userLimit;					  // Limit on the number of users in the channel
 
-	public:
-		//TODO (Alena): Function definition for 'Channel' not found.
-		//Channel();// Default constructor
-		
-		// TODO (Alena): commented out, _limit is not declared in Channel.cpp, causes error C/C++(292)
-		//Channel(const std::string& name); // Constructor with channel name
-		
-		//TODO (Alena): Function definition for '~Channel' not found.
-		//~Channel();// Destructor
-		
-		void addUser(Client* client);
-		void removeUser(Client* client);
-		void setTopic(const std::string& topic);
-		void setMode(char mode, bool enable);
-		void setKey(const std::string& key);
-		void setLimit(int _userLimit);
-		bool hasMembers(Client* client) const;
-		bool isOperator(Client* client) const;
+public:
+	// TODO (Alena): Function definition for 'Channel' not found.
+	// Channel();// Default constructor
+
+	// TODO (Alena): commented out, _limit is not declared in Channel.cpp, causes error C/C++(292)
+	// Channel(const std::string& name); // Constructor with channel name
+
+	// TODO (Alena): Function definition for '~Channel' not found.
+	//~Channel();// Destructor
+
+	void addUser(Client *client);
+	void removeUser(Client *client);
+	void setTopic(const std::string &topic);
+	void setMode(char mode, bool enable);
+	void setKey(const std::string &key);
+	void setLimit(int _userLimit);
+	bool hasMembers(Client *client) const;
+	bool isOperator(Client *client) const;
 };
 
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -6,66 +6,65 @@
 /*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:42:47 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/26 13:18:35 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/06/26 14:46:55 by akurmyza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Channel.hpp"
 #include "Client.hpp"
 
+// TODO (Alena):: commented out, "_limit" is not a nonstatic data member or base class of class "Channel"C/C++(292)
+// constructor with channel name
+// Channel::Channel(const std::string& name) : _name(name), _limit(0) {}
 
-//TODO (Alena):: commented out, "_limit" is not a nonstatic data member or base class of class "Channel"C/C++(292)
-//constructor with channel name
-//Channel::Channel(const std::string& name) : _name(name), _limit(0) {}
+// Function to add a user to the channel
+void Channel::addUser(Client *client)
+{
 
-
-//Function to add a user to the channel
-void Channel::addUser(Client* client){
-	
-	//TODO(Alena) : made tmp void for compiling ( unused parameter 'client')
+	// TODO(Alena) : made tmp void for compiling ( unused parameter 'client')
 	(void)client;
-	
-	//Add clients to the _members vector
+
+	// Add clients to the _members vector
 }
 
-//Function to remove a user from the channel
-void Channel::removeUser(Client* client){
-	
-		//TODO(Alena) : made tmp void for compiling ( unused parameter 'client')
-		(void)client;
-		
-	//Remove clients from the _members vector
+// Function to remove a user from the channel
+void Channel::removeUser(Client *client)
+{
+
+	// TODO(Alena) : made tmp void for compiling ( unused parameter 'client')
+	(void)client;
+
+	// Remove clients from the _members vector
 }
 
-//Function to set the topic of the channel
-void Channel::setTopic(const std::string& topic) {_topic = topic;}
+// Function to set the topic of the channel
+void Channel::setTopic(const std::string &topic) { _topic = topic; }
 
-//Function to set a mode for the channel
-//need to define a getter for the modes since is private attribute
-void Channel::setMode(char mode, bool enable) {_modes[mode] = enable;}
+// Function to set a mode for the channel
+// need to define a getter for the modes since is private attribute
+void Channel::setMode(char mode, bool enable) { _modes[mode] = enable; }
 
-//Function to set a key for the channel
-//need to define a getter for the key since is private attribute
-void Channel::setKey(const std::string& key) {_key = key;}
+// Function to set a key for the channel
+// need to define a getter for the key since is private attribute
+void Channel::setKey(const std::string &key) { _key = key; }
 
-//Function to set a limit for the channel
-//need to define a getter for the userlimit since is private attribute
-void Channel::setLimit(int limit) {_userLimit = limit;}
-
+// Function to set a limit for the channel
+// need to define a getter for the userlimit since is private attribute
+void Channel::setLimit(int limit) { _userLimit = limit; }
 
 /*
 TODO(Alena): commented out, because same function line 97 -115
    79 | bool Channel::hasMembers(Client* client) const{
-      |               ^
+	  |               ^
 src/Channel.cpp:56:15: note: previous definition is here
    56 | bool Channel::hasMembers(Client* client) const{
-      |               ^
+	  |               ^
 src/Channel.cpp:89:15: error: redefinition of 'isOperator'
    89 | bool Channel::isOperator(Client* client) const {
-      |               ^
+	  |               ^
 src/Channel.cpp:67:15: note: previous definition is here
    67 | bool Channel::isOperator(Client* client) const {
-      |               
+	  |
 
 
 
@@ -75,40 +74,51 @@ bool Channel::hasMembers(Client* client) const{
 
 		//TODO(Alena) : made tmp void for compiling ( unused parameter 'client')
 		(void)client;
-		
+
 	//return true if _client is in _members vector
-	
+
 	//TODO(Alena) : made tmp return true for compiling ( unused parameter 'client')
-    return true;
+	return true;
 }
 
 bool Channel::isOperator(Client* client) const {
 
 	//TODO(Alena) : made tmp void for compiling ( unused parameter 'client')
 	(void)client;
-	
+
 	//return true if _client is in _operators vector
-	
+
 		//TODO(Alena) : made tmp return true for compiling ( unused parameter 'client')
 		return true;
 }
 */
 
-//Function to check if a client is a member of the channel
-bool Channel::hasMembers(Client* client) const{
-	for(std::vector<Client*>::const_iterator it = _members.begin(); it != _members.end(); ++it){
-		if (*it == client){
+// Function to check if a client is a member of the channel
+bool Channel::hasMembers(Client *client) const
+{
+	for (std::vector<Client *>::const_iterator it = _members.begin(); it != _members.end(); ++it)
+	{
+		if (*it == client)
+		{
 			return true; // If the client is found in the members vector, return true
 		}
 		return false; // If the client is not found, return false
 	}
+
+	// TODO(Alena) : made tmp return true for compiling ( error: non-void function does not return a value in all control paths')
+	return true;
 }
 
-//Function to check if a client is an operator of the channel
-bool Channel::isOperator(Client* client) const {
-	for(std::vector<Client*>::const_iterator it = _operators.begin(); it != _operators.end(); ++it){
+// Function to check if a client is an operator of the channel
+bool Channel::isOperator(Client *client) const
+{
+	for (std::vector<Client *>::const_iterator it = _operators.begin(); it != _operators.end(); ++it)
+	{
 		if (*it == client)
 			return true; // If the client is found in the operators vector, return true
-		return false; // If the client is not found, return false
+		return false;	 // If the client is not found, return false
 	}
+	
+	// TODO(Alena) : made tmp return true for compiling ( error: non-void function does not return a value in all control paths')
+	return true;
 }
