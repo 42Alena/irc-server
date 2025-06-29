34f475a459b8431993b85870450b0f62406010d0
Alena <livionak@gmail.com>
2025-06-29 15:08:45 +0200

    Server:  added  sendToClient() , removeClient(fd)

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:31:22 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/27 17:34:38 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/06/29 14:14:59 by akurmyza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ private:
 
     // Connection & Client Management
     void acceptNewClient();
-    void removeClient(int fd);
+    void removeClient(int fd, size_t pollFdIndex);
     void handleClientInput(int fd);
     void sendToClient(int fd, const std::string &message);
 
