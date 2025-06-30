c3d19469a896dea4681eb2ab156ba9ec2a24348e
Numbersdontlie <luifer92@gmail.com>
2025-06-30 17:09:01 +0200

    Defined getters and setters for channel, still need to implement the rest of them

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: lperez-h <lperez-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:42:32 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/26 15:05:27 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/06/30 16:46:58 by lperez-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@
 #include "colors.hpp" /// Alena: replaced hardcoded color defines with colors.hpp
 #include <map>		  // Alena: added for map
 
+#include "../include/Server.hpp"
+#include "../include/Client.hpp"
+
 // TODO (Alena): added forward declaration Client class before use it
 class Client;
+
 class Channel
 {
 private:
@@ -33,22 +37,32 @@ private:
 
 public:
 	// TODO (Alena): Function definition for 'Channel' not found.
-	// Channel();// Default constructor
-
+	//Adding the Channel constructors and destructor
+	Channel();// Default constructor
+	Channel(const std::string& name); // Constructor with channel name
+	Channel(const Channel &other); // Copy constructor
+	Channel &operator=(const Channel &other); // Assignment operator
+	~Channel();// Destructor
+	
 	// TODO (Alena): commented out, _limit is not declared in Channel.cpp, causes error C/C++(292)
-	// Channel(const std::string& name); // Constructor with channel name
 
-	// TODO (Alena): Function definition for '~Channel' not found.
-	//~Channel();// Destructor
-
-	void addUser(Client *client);
-	void removeUser(Client *client);
+	void addUser(Client *client); //done
+	void removeUser(Client *client); //done
 	void setTopic(const std::string &topic);
 	void setMode(char mode, bool enable);
+	void setName(std::string name);
 	void setKey(const std::string &key);
 	void setLimit(int _userLimit);
 	bool hasMembers(Client *client) const;
 	bool isOperator(Client *client) const;
+
+	// Getters
+	std::string getName() const;
+	std::string getTopic() const;
+	std::string getKey() const;
+	int getLimit() const;
+	
+	
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: lperez-h <lperez-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:32:27 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/06/26 15:44:41 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/06/30 17:07:56 by lperez-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,17 @@ int main(int argc, char **argv)
 	}
 
 	std::cout << server << std::endl;
-	return EXIT_SUCCESS;
+	//return EXIT_SUCCESS;
+	
+	// Luis: I created this test to check if I can set limit to a channel and get it back, Feel free to remove it
+	// Create a channel
+    Channel channel;
+
+    // Set the user limit for the channel
+    channel.setLimit(10);
+
+    // Get and print the user limit
+    std::cout << "Channel user limit: " << channel.getLimit() << std::endl;
+
+    return 0;
 };
