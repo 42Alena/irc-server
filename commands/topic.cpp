b5c7252812d1f881f26c4b9563521d5b770aaab2
42Alena <livionak@gmail.com>
2025-07-17 22:42:37 +0200

    corrected topic channel

--- commands/topic.cpp
+++ commands/topic.cpp
@@ -6,36 +6,82 @@
 /*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 15:56:02 by lperez-h          #+#    #+#             */
+/*   Updated: 2025/07/17 22:39:32 by akurmyza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/commands.hpp"
 
-
 /*
-  Function to handle the change of the topic in the channel
-- it checks if the user is an operator before allowing them to change the topic
-- after this check if set the new topic and broadcasts a message to other members
+  Function to handle the change or query of the topic in a channel
+  - If only channel name is given: sends the current topic or "no topic" message.
+  - If topic is to be changed: checks if the user is allowed (based on +t mode).
+  - Broadcasts the new topic to all members.
 */
 void handleTopic(Server &server, Client &client, const std::vector<std::string> &params)
 {
 
-	if (params.size() < 2){
-		replyErr461NeedMoreParams(server.getServerName(), "TOPIC");
+	if (params.empty())
+	{
+		server.sendToClient(
+			client.getFd(),
+			replyErr461NeedMoreParams(server.getServerName(), "TOPIC"));
 		return;
 	}
-	std::string channelName = params[0];			   // Get the channel name from the parameters
-	std::string newTopic = params[1];				   // Get the new topic from the parameters
+	std::string channelName = params[0]; // Get the channel name from the parameters
+
 	Channel *channel = server.getChannel(channelName); // Get the channel by name from the server
-	if (!channel){
-		replyErr403NoSuchChannel(server.getServerName(), channelName);
+
+	if (!channel)
+	{
+		server.sendToClient(
+			client.getFd(),
+			replyErr403NoSuchChannel(server.getServerName(), channelName));
 		return;
 	}
-	if (!channel->isOperator(&client)){
-		replyErr482ChanOpPrivsNeeded(server.getServerName(), channelName);
+
+	if (!channel->hasMembers(&client))
+	{
+		server.sendToClient(
+			client.getFd(),
+			replyErr442NotOnChannel(server.getServerName(), channelName));
 		return;
 	}
-	channel->setTopic(newTopic);										   // Use the setter to change the topic of the channel
-	channel->sendToChannelExcept("Topic changed to: " + newTopic, client); // Notify other members
+
+	// Topic query (view current topic)
+	if (params.size() == 1)
+	{
+		if (channel->getTopic().empty())
+		{
+			server.sendToClient(
+				client.getFd(),
+				replyRpl331NoTopic(server.getServerName(), channelName, channel->getTopic()));
+		}
+		else
+		{
+			server.sendToClient(
+				client.getFd(),
+				replyRpl332Topic(server.getServerName(), channelName, channel->getTopic()));
+		}
+		return;
+	}
+
+	std::string newTopic = params[1]; // Get the new topic from the parameters
+
+	if (channel->isTopicLocked() && !channel->isOperator(&client))
+	{
+		server.sendToClient(
+			client.getFd(),
+			replyErr482ChanOpPrivsNeeded(server.getServerName(), channelName));
+		return;
+	}
+
+	channel->setTopic(newTopic); // Use the setter to change the topic of the channel
+
+	// Send proper IRC-formatted topic change message to ALL members
+	std::string msg = ":" + client.getPrefix() + " TOPIC " + channelName + " :" + newTopic;
+	channel->sendToChannelAll(msg, server);								   // requires sendToChannelAll(msg, server) to be implemented
+	
+	
 	logChannelInfo("Topic changed to: " + newTopic + " by " + client.getNickname());
 }
