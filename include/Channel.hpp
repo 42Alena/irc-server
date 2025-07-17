1014c126fdb4e8239c96ddc92408830da549c6a0
42Alena <livionak@gmail.com>
2025-07-17 21:30:21 +0200

    add sendToChannelAll(), fixed kick()

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 17:42:32 by akurmyza          #+#    #+#             */
-/*   Updated: 2025/07/17 15:38:56 by akurmyza         ###   ########.fr       */
+/*   Updated: 2025/07/17 21:26:58 by akurmyza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,23 @@ private:
 	Channel(const Channel &other);			  // Copy constructor
 	Channel &operator=(const Channel &other); // Assignment operator
 
-	//Alena: moved logs to utils.cpp
-	
+	// Alena: moved logs to utils.cpp
+
 public:
 	// Adding the Channel constructors and destructor
 	Channel();											 // Default constructor
 	Channel(const std::string &name, Client &createdBy); // Constructor with channel name and channel creator
 	~Channel();											 // Destructor
 
-	void addUser(int fd, Client *client);	 
-	void addOperator(int fd);				 
-	void removeUser(int fd, Client *client); 
+	void addUser(int fd, Client *client);
+	void addOperator(int fd);
+	void removeUser(int fd, Client *client);
 
-	bool hasMembers(Client *client) const; 
-	bool isOperator(Client *client) const; 
+	bool hasMembers(Client *client) const;
+	bool isOperator(Client *client) const;
 
-	void sendToChannelExcept(const std::string &message, const Client &clientExcluded) const; 
+	void sendToChannelAll(const std::string &message, Server &server);
+	void sendToChannelExcept(const std::string &message, const Client &clientExcluded) const;
 
 	void inviteUser(int fd);	  // Add user to invited list -> done
 	bool isInvited(int fd) const; // Check if user is invited -> done
@@ -68,21 +69,21 @@ public:
 	bool hasUserLimit() const;	  // Check if channel has a user limit set -> done
 
 	// Setters
-	void setTopic(const std::string &topic); 
-	void setMode(char mode, bool enable);	 
-	void setName(std::string name);			 
-	void setKey(const std::string &key);	 
-	void setLimit(int _userLimit);			 
+	void setTopic(const std::string &topic);
+	void setMode(char mode, bool enable);
+	void setName(std::string name);
+	void setKey(const std::string &key);
+	void setLimit(int _userLimit);
 
 	// Getters
-	std::string getName() const;				
+	std::string getName() const;
 	std::string getNickList() const;
-	std::string getTopic() const;				
-	std::string getPassword() const;			
+	std::string getTopic() const;
+	std::string getPassword() const;
 	std::map<int, Client *> getMembers() const;
-	std::set<int> getOperators() const;			
-	std::string getKey() const;					
-	int getLimit() const;						
+	std::set<int> getOperators() const;
+	std::string getKey() const;
+	int getLimit() const;
 };
 
 #endif
